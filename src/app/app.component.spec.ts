bd11609a5429c53bb791724f3de4e7adec75819a
Martijn Lutgens <info@lutsoft.nl>
2021-08-04 20:19:21 +0200

    make scoreboard

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'scoreboard'`, () => {
+  it(`should have score 1 as 0`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('scoreboard');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('scoreboard app is running!');
+    expect(app.score1).toEqual(0);
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'scoreboard';
+  score1 = 0;
+  score2 = 0;
+
+  add1() {
+    if(this.score1<999) {
+      this.score1++;
+    }
+  }
+
+  min1() {
+    if(this.score1>0) {
+      this.score1--;
+    }
+  }
+  add2() {
+    if(this.score2<999) {
+      this.score2++;
+    }
+  }
+
+  min2() {
+    if(this.score2>0) {
+      this.score2--;
+    }
+  }
 }
