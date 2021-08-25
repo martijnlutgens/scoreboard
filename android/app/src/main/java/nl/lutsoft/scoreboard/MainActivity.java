9f3e8391808b42f0abfacf950a82651c6476e0e1
Yannicklutgens <yannicklutgens@gmail.com>
2021-08-25 20:38:10 +0200

    made apk

---
+++ android/app/src/main/java/nl/lutsoft/scoreboard/MainActivity.java
@@ -0,0 +1,5 @@
+package nl.lutsoft.scoreboard;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'nl.lutsoft.scoreboard',
+  appName: 'scoreboard',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
